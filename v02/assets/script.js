097c219f0f5e5e690d9385e026c12b1d2ca66346
Kirill Korolev <102628920+kkorolevdev@users.noreply.github.com>
2024-03-20 23:00:52 +0300

    feat: init v2

---
+++ v02/assets/script.js
@@ -0,0 +1,20 @@
+(function () {
+
+	// Settings.
+	const mathExercises = 10;
+
+	let result;
+	const resultMin = 0;
+	const resultMax = 10;
+	
+	const actionOne = ['add', 'substract'];
+	
+	let paramOne;
+	const paramOneMin = 1;
+	const paramOneMax = 9;
+
+	let paramTwo;
+	const paramTwoMin = 1;
+	const paramTwoMax = 9;
+
+})();
